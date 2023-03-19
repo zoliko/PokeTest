5aa6ba3720b28b7f9fb06db73f13069e5f08a8aa
Marcelo <marcelo.aguilar@elekra.com.mx>
2023-03-19 07:39:45 -0600

    checkpoint

---
+++ Main/Main/View/Enrollment/View/EnrollmentViewController.swift
@@ -0,0 +1,32 @@
+//
+//  EnrollmentViewController.swift
+//  Main
+//
+//  Created by Marcelo Aguilar Bravo on 19/03/23.
+//
+
+import UIKit
+import DITPVisualComponents
+
+class EnrollmentViewController: UIViewController {
+
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var pokeballRotate: UIImageView!
+    @IBOutlet weak var pokeball: UIImageView!
+    
+    var presenter: EnrollmentPresenterProtocol?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpView()
+        presenter?.viperDidLoad()
+    }
+    
+    func setUpView() {
+        pokeballRotate.image = UIImage(named: "DIVCShadowballrotate", in: DITPVisualComponentsBundleManager.bundle(), compatibleWith: nil)
+        logo.image = UIImage(named: "DIVCLogo", in: DITPVisualComponentsBundleManager.bundle(), compatibleWith: nil)
+        view.addGradientEfect(fistColor: .primaryBlue, secondColor: .secondaryBlue)
+        pokeball.image = UIImage(named: "DIVCShadowball", in: DITPVisualComponentsBundleManager.bundle(), compatibleWith: nil)?.withTintColor(.primaryWhite.withAlphaComponent(0.05), renderingMode: .alwaysOriginal)
+    }
+
+}
