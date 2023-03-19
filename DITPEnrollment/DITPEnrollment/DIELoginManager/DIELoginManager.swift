44a36a2d7d87d782e461cb88d9bff409f0e6552e
Marcelo <marcelo.aguilar@elekra.com.mx>
2023-03-19 14:58:17 -0600

    se crea framework DITPEnrollment

---
+++ DITPEnrollment/DITPEnrollment/DIELoginManager/DIELoginManager.swift
@@ -0,0 +1,36 @@
+//
+//  DIELoginManager.swift
+//  DITPEnrollment
+//
+//  Created by Marcelo Aguilar Bravo on 19/03/23.
+//
+
+import Foundation
+
+public class DIELoginManager {
+    
+    private static var userDefaults: UserDefaults = UserDefaults.standard
+    
+    private static func saveObject(withKey key: String, object: Any ) {
+        userDefaults.set(object, forKey: key)
+    }
+    private static func recoverObject(withKey key: String) -> Any? {
+        let object = userDefaults.value(forKey: key)
+        return object
+    }
+    
+    public static func loggIn() {
+        saveObject(withKey: "DIELoginKey", object: true)
+    }
+    public static func loggOut() {
+        saveObject(withKey: "DIELoginKey", object: false)
+    }
+    public static func isLoggin() -> Bool {
+        guard let logginState = recoverObject(withKey: "DIELoginKey") as? Bool
+        else {
+            return false
+        }
+        return logginState
+    }
+    
+}
---
+++ DITPEnrollment/DITPEnrollment/DITPEnrollment.h
@@ -0,0 +1,18 @@
+//
+//  DITPEnrollment.h
+//  DITPEnrollment
+//
+//  Created by Marcelo Aguilar Bravo on 19/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DITPEnrollment.
+FOUNDATION_EXPORT double DITPEnrollmentVersionNumber;
+
+//! Project version string for DITPEnrollment.
+FOUNDATION_EXPORT const unsigned char DITPEnrollmentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DITPEnrollment/PublicHeader.h>
+
+
