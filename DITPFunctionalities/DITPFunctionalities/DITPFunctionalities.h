929ab30cb14eb9b63352c5dd209d3bc8f1f145e6
Marcelo <marcelo.aguilar@elekra.com.mx>
2023-03-19 00:47:50 -0600

    primer  Commit

---
+++ DITPFunctionalities/DITPFunctionalities/DITPFunctionalities.h
@@ -0,0 +1,18 @@
+//
+//  DITPFunctionalities.h
+//  DITPFunctionalities
+//
+//  Created by Marcelo Aguilar Bravo on 19/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DITPFunctionalities.
+FOUNDATION_EXPORT double DITPFunctionalitiesVersionNumber;
+
+//! Project version string for DITPFunctionalities.
+FOUNDATION_EXPORT const unsigned char DITPFunctionalitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DITPFunctionalities/PublicHeader.h>
+
+
---
+++ Main/Main/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Main
+//
+//  Created by Marcelo Aguilar Bravo on 19/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
